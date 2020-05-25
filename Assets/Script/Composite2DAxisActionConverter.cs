ee410c7d4c1f5418d601560bd31cf05aa6d19fdf
Zack Smith <947906+zackintosh@users.noreply.github.com>
2020-05-24 18:19:28 -0600

    First pass at mapping the Midi Fighter 3D

---
+++ Assets/Script/Composite2DAxisActionConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace Zack
+{
+    [System.Serializable] public sealed class Vector2Event : UnityEvent<Vector2> { };
+
+    public class Composite2DAxisActionConverter : MonoBehaviour
+    {
+        public Vector2Event eventToFire = null;
+
+        public void OnAction(InputAction.CallbackContext context)
+        {
+            var value = context.ReadValue<Vector2>();
+            //Debug.Log(value);
+
+            eventToFire?.Invoke(value);
+        }
+    }
+}
---
+++ Assets/Script/RotateBasedOnComposite2DAxis.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBasedOnComposite2DAxis : MonoBehaviour
+{
+    public Vector3 xMap = new Vector3(0.0f, -1.0f, 0.0f);
+    public Vector3 yMap = new Vector3(1.0f, 0.0f, 0.0f);
+    public float angleScale = 90.0f;
+
+    public void RotateVector2(Vector2 vector)
+    {
+        var angle = ((xMap * vector.x) + (yMap * vector.y)) * angleScale;
+        transform.localRotation = Quaternion.Euler(angle);
+    }
+}
